f06abdf0f735427fd78aed733c5dd95ebbbf776d
Jouramie <16137441+Jouramie@users.noreply.github.com>
2025-05-11 15:07:32 -0400

    tune rush hours and post tasks

--- test/sensor_test.py
+++ test/sensor_test.py
@@ -109,3 +109,25 @@ def test_medium_grape_w_flavor_blast():
 
     finally:
         img_logger.finalize()
+
+
+def test_the_triple_w_bacon_2_rush_hour_1():
+    try:
+        img = cv2.cvtColor(cv2.imread(r"resources/the_triple_w_bacon_2_rush_hour_1.tiff"), cv2.COLOR_BGR2RGB)
+
+        active_task = sensor.read_task_statement(img, log_steps="the_triple_w_bacon_2_rush_hour_1")
+        assert active_task is None
+
+    finally:
+        img_logger.finalize()
+
+
+def test_the_triple_w_bacon_2_rush_hour_2():
+    try:
+        img = cv2.cvtColor(cv2.imread(r"resources/the_triple_w_bacon_2_rush_hour_2.tiff"), cv2.COLOR_BGR2RGB)
+
+        active_task = sensor.read_task_statement(img, log_steps="the_triple_w_bacon_2_rush_hour_2")
+        assert active_task is None
+
+    finally:
+        img_logger.finalize()
