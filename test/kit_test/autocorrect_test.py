96e577fbdff191613f5af31c601cd0dbbf66c6c2
Jouramie <16137441+Jouramie@users.noreply.github.com>
2025-10-20 19:56:56 -0400

    stuff

--- test/kit_test/autocorrect_test.py
+++ test/kit_test/autocorrect_test.py
@@ -1,3 +1,5 @@
+from unittest import skip
+
 from botkit.text import autocorrect
 from core import brain
 
@@ -16,6 +18,17 @@ def test_cherry_vanilla():
     text = "Two Vanilla Scoops with a Cherry, please."
     dictionary = set(brain.EQUIPMENTS["Ice Cream"].steps[0].autocorrect_dictionary)
     expected = "two vanilla scoops with a cherry please"
+
+    corrected = autocorrect(text, dictionary)
+
+    assert corrected == expected
+
+
+@skip("Onions is too far from Onidhy in terms of edit distance.")
+def test_olives_and_onions_pizza():
+    text = "Tomato Sauce, Cheese, Olives and Onidhy"
+    dictionary = set(brain.EQUIPMENTS["Pizza"].steps[0].autocorrect_dictionary)
+    expected = "tomato sauce cheese olives and onions"
 
     corrected = autocorrect(text, dictionary)
 
