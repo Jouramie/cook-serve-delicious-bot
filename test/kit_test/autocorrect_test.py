47b94b37d9836d2e7cb983c5582efd57c996968e
Jouramie <16137441+Jouramie@users.noreply.github.com>
2026-03-08 19:45:16 -0400

    add even more autocorrent resilienccy

--- test/kit_test/autocorrect_test.py
+++ test/kit_test/autocorrect_test.py
@@ -1,7 +1,12 @@
-from unittest import skip
+import logging
 
+import pytest
+
+import suite_utils
 from botkit.text import autocorrect
 from core import brain
+
+suite_utils.enable_stdout_logs(logging.DEBUG)
 
 
 def test_tombo_jumbo():
@@ -24,11 +29,21 @@ def test_cherry_vanilla():
     assert corrected == expected
 
 
-@skip("Onions is too far from Onidhy in terms of edit distance.")
+@pytest.mark.skip(reason="Onions is too far from Onidhy in terms of edit distance.")
 def test_olives_and_onions_pizza():
     text = "Tomato Sauce, Cheese, Olives and Onidhy"
     dictionary = set(brain.EQUIPMENTS["Pizza"].steps[0].autocorrect_dictionary)
     expected = "tomato sauce cheese olives and onions"
+
+    corrected = autocorrect(text, dictionary)
+
+    assert corrected == expected
+
+
+def test_bacon_extra_brocoli_salad():
+    text = "Vinaigrette, Cheese, � zon and Croutons."
+    dictionary = set(brain.EQUIPMENTS["Salad"].steps[0].autocorrect_dictionary)
+    expected = "vinaigrette cheese bacon and croutons"
 
     corrected = autocorrect(text, dictionary)
 
